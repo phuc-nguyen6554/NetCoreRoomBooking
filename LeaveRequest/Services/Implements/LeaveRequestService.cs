5a6f930604b4d9cd2d55782c2ce33b7f6fbe04a3
gabriel-smc <gabriel.nguyen@dealersolutions.com.au>
2020-09-23 16:58:15 +0700

    leave request list backend

--- LeaveRequest/Services/Implements/LeaveRequestService.cs
+++ LeaveRequest/Services/Implements/LeaveRequestService.cs
@@ -27,27 +27,50 @@ namespace LeaveRequest.Services.Implements
         }
 
         //get leave request
-        public async Task<List<LeaveRequestResponse>> GetLeaveRequestAsync(LeaveRequestListRequest b)
+        public async Task<List<LeaveRequestResponse>> GetLeaveRequestAsync(LeaveRequestListRequest request)
         {
             var leave = new List<Request>();
      
-            if (b.LeaveDates != null)
+            if (request.LeaveDates != null)
             {
-                leave = await _context.LeaveRequests.Include(b => b.LeaveTypes).Where(x => x.LeaveDates == b.LeaveDates).ToListAsync();
+                //admin can list all leave request
+                if (_cache.Role == Constrain.AdminRole)
+                {
+                    leave = await _context.LeaveRequests.Include(b => b.LeaveTypes).Where(x => x.LeaveDates.Date == request.LeaveDates.Value.Date).ToListAsync();
+                }
+                //user can list of his/here leave request
+                else
+                { 
+                    leave = await _context.LeaveRequests.Include(b => b.LeaveTypes).Where(l => l.MemberEmail == _cache.Email).Where(x => x.LeaveDates.Date == request.LeaveDates.Value.Date).ToListAsync();
+                }
             }
             
-            if (b.LeaveTime != null)
+            if (request.LeaveTime != null)
             {
-                leave = await _context.LeaveRequests.Include(b => b.LeaveTypes).Where(x => x.LeaveTime == b.LeaveTime).ToListAsync();
+                leave = await _context.LeaveRequests.Include(b => b.LeaveTypes).Where(x => x.LeaveTime == request.LeaveTime).ToListAsync();
             }
             
-            if (b.LeaveTypeId != 0)
+            if (request.LeaveTypeId != 0)
             {
-                leave = await _context.LeaveRequests.Include(x => x.LeaveTypes).Where(x => x.LeaveTypeId == b.LeaveTypeId).ToListAsync();
+                leave = await _context.LeaveRequests.Include(x => x.LeaveTypes).Where(x => x.LeaveTypeId == request.LeaveTypeId).ToListAsync();
             }
-            else if((b.LeaveTime == null) && (b.LeaveTypeId == 0) && (b.LeaveDates == null))
+            //get all leave type
+            if (request.LeaveTypeId == -1)
             {
-                leave = await _context.LeaveRequests.Include(b => b.LeaveTypes).ToListAsync();
+                leave = await _context.LeaveRequests.Include(x => x.LeaveTypes).ToListAsync();
+            }
+            else if((request.LeaveTime == null) && (request.LeaveTypeId == 0) && (request.LeaveDates == null))
+            {
+                //admin can list all leave request
+                if (_cache.Role == Constrain.AdminRole)
+                {
+                    leave = await _context.LeaveRequests.Include(b => b.LeaveTypes).ToListAsync();
+                }
+                //user can list of his/here leave request
+                else
+                {
+                    leave = await _context.LeaveRequests.Where(l => l.MemberEmail == _cache.Email).Include(b => b.LeaveTypes).ToListAsync();
+                }
             }
             
             return _mapper.Map<List<LeaveRequestResponse>>(leave);
@@ -60,7 +83,7 @@ namespace LeaveRequest.Services.Implements
             
             if (leave.MemberEmail != _cache.Email && _cache.Role != Constrain.AdminRole)
             {
-                throw new ServiceException(401, "You don't have permission to delete this Booking");
+                throw new ServiceException(401, "You don't have permission to delete this Leave Request");
             }
 
             _context.LeaveRequests.Remove(leave);
